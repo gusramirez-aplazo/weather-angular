2e70136088984af54f4124db9ab3c961a708bc34
Gus Ramírez <gustavo@aplazo.mx>
2023-12-08 11:57:32 -0600

    chore: remove unused code and set scaffolding

---
+++ src/app/features/home/home.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-home',
+  template: ` <h1 class="text-lg font-medium ">Search by city</h1> `,
+})
+export class HomeComponent {}
